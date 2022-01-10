5baad2f2f70b98adaaf8f540314fd3d3105db91b
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-01-10 15:24:06 -0300

    Added client contract test

--- flux-flix-client/src/test/java/com/example/reactivedata/MovieWebClientTest.java
+++ flux-flix-client/src/test/java/com/example/reactivedata/MovieWebClientTest.java
@@ -1,57 +1,35 @@
 package com.example.reactivedata;
 
-import java.util.List;
 import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.tomakehurst.wiremock.client.WireMock;
 
 import reactor.test.StepVerifier;
 
 @ExtendWith(SpringExtension.class)
-@AutoConfigureWireMock(port = 8080)
-@AutoConfigureJson
+@AutoConfigureStubRunner(
+	stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+	ids = "com.example:flux-flix-service:+:8080"
+)
 @SpringBootTest
 class MovieWebClientTest {
 	
 	@Autowired
 	private MovieWebClient client;
-	
-	@Autowired
-	private ObjectMapper objectMapper;
 
 	@Test
 	public void test() throws Exception {
 		
-		var movies = List.of(
-				new Movie(null, "Hello World", "Horror"), 
-				new Movie(null, "Here I am", "Comedy"));
-		
-		var json = objectMapper.writeValueAsString(movies);
-		
-		WireMock.stubFor(
-				WireMock.get(WireMock.urlEqualTo("/movies"))
-			.willReturn(
-					WireMock.aResponse()
-						.withBody(json)
-						.withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-						.withStatus(HttpStatus.OK.value())));
-		
 		StepVerifier
 			.create(this.client.getAllMovies())
 			.expectNextMatches(predicate(null, "Hello World", "Horror"))
-			.expectNextMatches(predicate(null, "Here I am", "Comedy"))
+			.expectNextMatches(predicate(null, "Look at me", "Drama"))
 			.verifyComplete();
 	}
 	
