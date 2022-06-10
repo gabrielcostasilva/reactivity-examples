16937842a6202b225541752660b232a834ae00f6
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2022-06-10 18:43:49 -0300

    Added Todo controller

---
+++ reactive-todo/src/main/java/edu/utfpr/cp/esp/java/reactivetodo/TodoRestController.java
@@ -0,0 +1,59 @@
+package edu.utfpr.cp.esp.java.reactivetodo;
+
+import java.time.Duration;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class TodoRestController {
+
+    private final TodoRepository repository;
+
+    public TodoRestController (TodoRepository repository) {
+        this.repository = repository;
+    }
+    
+    @GetMapping("/todos")
+    public Flux<Todo> lerTodos() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/todos/{feito}")
+    public Flux<Todo> lerByFeito(@PathVariable boolean feito) {
+        return repository.findByFeito(feito);
+    }
+
+    @PostMapping("/todo")
+    public Mono<Todo> criar(@RequestBody Todo todo) {
+        return repository.save(todo);
+    }
+
+    @DeleteMapping("/todo/{id}")
+    public Mono<Void> deletar(@PathVariable String id) {
+        return repository.deleteById(id);
+    }
+
+    @PutMapping("/todo/{id}")
+    public Mono<Todo> atualizar(@PathVariable String id) {
+
+        return repository
+                    .findById(id)
+                    .map(todoAtual ->  new Todo(id, 
+                                                todoAtual.titulo(), 
+                                                todoAtual.descricao(), 
+                                                !todoAtual.feito()))
+                    .flatMap(repository::save)
+                    .onTerminateDetach();
+    }
+
+    
+}
